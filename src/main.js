a31d8fc48f824dbfcc01feff3e6175f8692e6b72
Melissa E. Chavez <MChavez905@cnm.edu>
2024-08-09 17:07:55 -0600

    PWP added Nav, Hero sect., Acct services and image

--- src/main.js
+++ src/main.js
@@ -1,3 +1,13 @@
 import './index.css'
 import 'flowbite'
 
+let currentIndex=0;
+    const slides = document.querySelectorAll('.hero-slider > div');
+    const totalSlides = slides.length
+
+function showNextSlide() {
+        currentIndex=(currentIndex + 1) % totalSlides;
+        document.querySelector('.hero-slider').style.trasform= `translateX(-${currentIndex *100}%)`;
+}
+
+setTimeout(showNextSlide, 3000);
